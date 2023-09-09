8acd004b80cecd23c29069b99af9091fe70e8edc
Marjory Washio <marjorywashio@hotmail.com>
2023-09-09 12:54:16 -0300

    calculadora

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+print("---------- Calculadora de Juros Simples ----------\n")
+
+valor = float(input("Digite o valor principal: "))
+
+juros = float(input("Digite a taxa de juros anual: "))
+
+tempo = int(input("Digite o tempo em anos: "))
+
+juros1 = juros/100
+
+montante = valor + (valor * juros1 * tempo)
+
+print("\n---------- Resultado ----------")
+print(f"Com o valor principal de R${valor: .2f}, ao final de {tempo} anos, com juros de{juros: .2f}% ao ano, o montante total é de R${montante: .2f}")
