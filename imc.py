e1564599f99673ea80b1d52b71f3e9353be45b50
Marjory Washio <marjorywashio@hotmail.com>
2023-09-09 12:16:23 -0300

    estética

--- imc.py
+++ imc.py
@@ -1,4 +1,4 @@
-print("Seja bem vindo(a)!! \n")
+print("Seja bem vindo(a)!! Vamos calcular seu IMC? \n")
 
 nome = input("Qual é o seu nome? ")
 
@@ -6,10 +6,11 @@ idade = int(input("Qual é a sua idade? ")) #converter para inteiro
 
 peso = float(input("Qual é o seu peso? ")) #converter para float
 
-altura = float(input("Qual é a sua altura? ")) #converter para float
+altura = float(input("Qual é a sua altura? ")) #converter para float / utilizar . (1.62)
 
 imc = peso/(altura*altura)
 
+print("\n------- Resultados -------")
 print(f"Olá {nome}!")
 print(f"Sua idade é de {idade} anos.")
 print(f"Seu IMC é de:{imc: .2f}.")
