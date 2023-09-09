c80f3c7d2e3d38929d38ef4e04ea7297aed72ea1
Marjory Washio <marjorywashio@hotmail.com>
2023-09-09 10:50:50 -0300

    Meu arquivo Hello World

---
+++ hello.py
@@ -0,0 +1,13 @@
+print ('Hello world!')
+
+nome = "Marjory"
+profissao = "estudante"
+
+#aspas dupla e 'f' fora das aspas
+print(f"Este é o primeiro programa de {nome}.") 
+
+#forma de fazer concatenação: entre vírgulas
+print("Eu sou", profissao, "e decidi trilhar os caminhos do Python.")
+
+#função input: parar a execução do programa e aguardar resposta do usuário
+resposta = input("Você está pronto para vir comigo? (S/N)")
