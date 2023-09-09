9dd53f914ed5920dce2f1ee74b7acd0eb4e9aaa6
Marjory Washio <marjorywashio@hotmail.com>
2023-09-09 16:53:12 -0300

    calculadora

---
+++ calculadorasimples.py
@@ -0,0 +1,19 @@
+print("------- VERIFICAÇÃO DE MAIORIDADE -------\n")
+
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+operacao = input("Digite a operação que deseja realizar: ")
+
+if operacao == "+":
+    resultado = num1 + num2
+    print(f"O resultado é {resultado}")
+elif operacao == "-":
+    resultado = num1 - num2
+    print(f"O resultado é {resultado}")
+elif operacao == "/":
+    resultado = num1 / num2
+    print(f"O resultado é {resultado}")
+elif operacao == "*":
+    resultado = num1 * num2
+    print(f"O resultado é {resultado}")
+
--- idade.py
+++ idade.py
@@ -5,7 +5,7 @@ print("------- VERIFICAÇÃO DE MAIORIDADE -------\n")
 
 idade = int(input("Qual é a sua idade? "))
 
-if (idade >= 18):
+if idade >= 18:
     print("Maior de idade")
 else:
     print("Menor de idade")
