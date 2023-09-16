46288bfd733a1f85f3ea0619d9bafa328df6919e
Marjory Washio <marjorywashio@hotmail.com>
2023-09-16 10:51:12 -0300

    melhoria do código

--- notas.py
+++ notas.py
@@ -2,14 +2,20 @@
 # "A" (90-100), "B" (80-89), "C" (70-79), "D" (60-69) ou "E" (abaixo de 60).
 
 nota = float(input("Digite uma nota de 0 a 100: "))
+classificação = ''
 
-if nota > 90:
-    print("Classificação da nota: A")
-elif nota > 80 and nota < 89:
-    print("Classificação da nota: B")
-elif nota > 70 and nota < 79:
-    print("Classificação da nota: C")
-elif nota > 60 and nota < 69:
-    print("Classificação da nota: D")
-elif nota < 60:
-    print("Classificação da nota: E")
+if nota >=0 and nota <=100: 
+    if nota >= 90:
+        classificação = "A"
+    elif nota >= 80 and nota <= 89:
+        classificação = "B"
+    elif nota >= 70 and nota <= 79:
+        classificação = "C"
+    elif nota >= 60 and nota <= 69:
+        classificação = "D"
+    else:
+        classificação = "E"
+    print(f"Classificação da nota: {classificação}")
+else:
+    print("Nota fora do parâmetro pedido (de 0 a 100)")
+
