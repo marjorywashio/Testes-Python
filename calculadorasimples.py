57330c37fa4912032987664798f8ad98b5773319
Marjory Washio <marjorywashio@hotmail.com>
2023-09-16 10:08:43 -0300

    edição do código

--- calculadorasimples.py
+++ calculadorasimples.py
@@ -1,21 +1,25 @@
 print("------- VERIFICAÇÃO DE MAIORIDADE -------\n")
 
-num1 = float(input("Digite o primeiro número: "))
-num2 = float(input("Digite o segundo número: "))
-operacao = input("Digite a operação que deseja realizar (+, -, / ou *): ")
+num1 = float(input("Primeiro número: "))
+operacao = input("Operação(+ - / *): ")
+num2 = float(input("Segundo número: "))
+
+resultado = ''
 
 if operacao == "+":
     resultado = num1 + num2
-    print(f"O resultado é {resultado}")
 elif operacao == "-":
     resultado = num1 - num2
-    print(f"O resultado é {resultado}")
 elif operacao == "/":
-    resultado = num1 / num2
-    print(f"O resultado é {resultado}")
+    if num2 == 0:
+        print("Não é possível dividir por zero")
+    else:
+        resultado = num1 / num2
 elif operacao == "*":
     resultado = num1 * num2
-    print(f"O resultado é {resultado}")
 else:
     print("Operação não encontrada. Repita o cálculo.")
+
+if resultado != '':
+    print(f"O resultado é {resultado}")
 
