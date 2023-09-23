fef45b4bb2143c66f4015c6bc2a78789c144df56
Marjory Washio <marjorywashio@hotmail.com>
2023-09-23 10:25:15 -0300

    for + while

--- lacos_teste.py
+++ lacos_teste.py
@@ -14,7 +14,7 @@ numero = 0
 # - Escreve um programa que imprima números de 1 a 10 usando o laço for
 # - Refatore o código para solicitar um número máximo para impressão ao usuário
 # - Refatore o código para solicitar ao usuário se deve ser impresso em ordem crescente ou descrescente
-maximo = int(input("Até qual número você gostaria de imprimir? "))
+numero = maximo = int(input("Até qual número você gostaria de imprimir? "))
 ordem = int(input("Gostaria de ordenar de forma crescente (1) ou decrescente (2)? "))
 
 if ordem == 1:
@@ -23,5 +23,19 @@ if ordem == 1:
 elif ordem == 2:
     for numero in range (maximo, 0, -1):
         print(numero)
+else:
+    print("\nEscolha a ordenação com os números 1 ou 2")
+
+print("==========WHILE============")
+
+if ordem == 1:
+    numero = 1
+    while numero <= maximo:
+        print(numero)
+        numero +=1
+elif ordem == 2:
+    while maximo > 0:
+        print(maximo)
+        maximo -= 1
 else:
     print("\nEscolha a ordenação com os números 1 ou 2")
