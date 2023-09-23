bd2ff32df491e0dfe9f508cefb5f4955a269d7ec
Marjory Washio <marjorywashio@hotmail.com>
2023-09-23 10:10:57 -0300

    laço for

---
+++ lacos_teste.py
@@ -0,0 +1,27 @@
+numero = 0
+
+#while numero < 5:
+#    numero += 1
+#    print(numero)
+
+#for numero in [1, 2, 3, 4, 5]:
+#    print(numero)
+
+#for numero in [1,2,3,4,5,6,7,8,9,10]:
+#    print(numero)
+
+#     Exercício 1 - Impressão de números
+# - Escreve um programa que imprima números de 1 a 10 usando o laço for
+# - Refatore o código para solicitar um número máximo para impressão ao usuário
+# - Refatore o código para solicitar ao usuário se deve ser impresso em ordem crescente ou descrescente
+maximo = int(input("Até qual número você gostaria de imprimir? "))
+ordem = int(input("Gostaria de ordenar de forma crescente (1) ou decrescente (2)? "))
+
+if ordem == 1:
+    for numero in range(1, maximo + 1):
+        print(numero)
+elif ordem == 2:
+    for numero in range (maximo, 0, -1):
+        print(numero)
+else:
+    print("\nEscolha a ordenação com os números 1 ou 2")
--- lojaroupa.py
+++ lojaroupa.py
@@ -7,62 +7,55 @@ pagamento = input("Qual a forma de pagamento? ")
 
 if pagamento == "v":
     if compra <= 500:
-        total = (compra*0.90) #10% de desconto
-        print(f"O total da compra é R${total: .2f}")
-    if compra > 500 and compra < 1000:
-        total = compra*0.85 #15% de desconto
-        print(f"O total da compra é R${total: .2f}")
-    if compra >= 1000:
-        total = compra*0.80 #20% de desconto
-        print(f"O total da compra é R${total: .2f}")
+        total = float(compra*0.90) #10% de desconto
+    elif compra > 500 and compra < 1000:
+        total = float(compra*0.85) #15% de desconto
+    else:
+        total = float(compra*0.80) #20% de desconto
 
-if pagamento =="p":
+    print(f"O total da compra é R${total: .2f}")
+
+elif pagamento =="p":
     if compra <= 800:
         print("\nA compra pode ser parcelada em até 4x")
         parcelas = int(input("Gostaria de parcelar em quantas vezes? "))
+
         if parcelas <= 4:
             total = compra / parcelas
             print(f"\nCompra parcelada em {parcelas} vezes de R${total: .2f}")
         else:
             print("A compra pode ser parcelada em no máximo 4x!")
-    if compra > 800:
+
+    elif compra > 800:
         print("\nA compra pode ser parcelada em até 18x")
         print("----- Até 10x sem juros -----")
         parcelas = int(input("\nGostaria de parcelar em quantas vezes? "))
-        if parcelas < 11:
-            total = compra/parcelas
-            print(f"Compra parcelada em {parcelas} vezes de R${total: .2f} sem juros")
-        if parcelas == 11:
-            juros = 0.05
-            total = compra + (compra * juros * parcelas)
-            print(f"\nCompra parcelada em {parcelas} vezes de R${total/parcelas: .2f}")
-        elif parcelas == 12:
-            juros = 0.065
-            total = compra + (compra * juros * parcelas)
-            print(f"\nCompra parcelada em {parcelas} vezes de R${total/parcelas: .2f}")
-        elif parcelas == 13:
-            juros = 0.07
-            total = compra + (compra * juros * parcelas)
-            print(f"\nCompra parcelada em {parcelas} vezes de R${total/parcelas: .2f}")
-        elif parcelas == 14:
-            juros = 0.09
-            total = compra + (compra * juros * parcelas)
-            print(f"\nCompra parcelada em {parcelas} vezes de R${total/parcelas: .2f}")
-        elif parcelas == 15:
-            juros = 0.095
-            total = compra + (compra * juros * parcelas)
-            print(f"\nCompra parcelada em {parcelas} vezes de R${total/parcelas: .2f}")
-        elif parcelas == 16:
-            juros = 0.1
-            total = compra + (compra * juros * parcelas)
-            print(f"\nCompra parcelada em {parcelas} vezes de R${total/parcelas: .2f}")
-        elif parcelas == 17:
-            juros = 0.113
-            total = compra + (compra * juros * parcelas)
-            print(f"\nCompra parcelada em {parcelas} vezes de R${total/parcelas: .2f}")
-        elif parcelas == 18:
-            juros = 0.12
-            total = compra + (compra * juros * parcelas)
-            print(f"\nCompra parcelada em {parcelas} vezes de R${total/parcelas: .2f}")
-        else:
+
+        if parcelas > 18:
             print("\nA compra pode ser parcelada em até 18x")
+        else:
+            if parcelas <= 10:
+                total = compra/parcelas
+                print(f"Compra parcelada em {parcelas} vezes de R${total: .2f} sem juros")
+            elif parcelas == 11:
+                juros = 0.05
+            elif parcelas == 12:
+                juros = 0.065
+            elif parcelas == 13:
+                juros = 0.07
+            elif parcelas == 14:
+                juros = 0.09
+            elif parcelas == 15:
+                juros = 0.095
+            elif parcelas == 16:
+                juros = 0.1
+            elif parcelas == 17:
+                juros = 0.113
+            elif parcelas == 18:
+                juros = 0.12
+
+            total = compra + (compra * juros * parcelas)
+            print(f"\nCompra parcelada em {parcelas} vezes de R${total/parcelas: .2f}")
+
+else:
+    print("Tipo de pagamento não reconhecido.")
